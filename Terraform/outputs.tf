b68887c65f7f7cc80775f963a1480a8fadd12bda
queensley200 <105300323+queensley200@users.noreply.github.com>
2024-09-24 16:33:38 +0100

    Update outputs.tf
    i had to rename the ec2 output

--- Terraform/outputs.tf
+++ Terraform/outputs.tf
@@ -2,7 +2,8 @@ output "vpc_id" {
   value = module.vpc.vpc_id
 }
 
-output "ec2_public_ip" {
-  value = module.ec2.public_ip
+output "ec2_public_ip_output" {
+    value = aws_instance.my_ec2.public_ip
 }
+
 
